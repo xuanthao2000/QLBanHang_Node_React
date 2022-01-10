03db334cf518834ec8a3b97cad5b33b255cddb78
1851010123 <1851010123thao@ou.edu.vn>
2022-01-10 21:27:45 +0700

    set up enviroment

---
+++ backend/src/app.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+
+
+app.listen(port, () => console.log(`Listening at http://localhost:3000`));
+
---
+++ fontend/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <p>This is font end</p>
+  );
+}
+
+export default App;
